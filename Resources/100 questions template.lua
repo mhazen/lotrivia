ec66a12bff9f1c08b82f7a3773dc8cc37da1ec6f
Mark Hazen <markh@markh.com>
2013-11-22 16:10:55 -0500

    adding test questions pack and temnplate for adding 100 more questions

---
+++ Resources/100 questions template.lua
@@ -0,0 +1,400 @@
+LT_Question[300] = ""
+LT_Answer[300] = ""
+LT_QuestionCommentary[300] = ""
+
+LT_Question[301] = ""
+LT_Answer[301] = ""
+LT_QuestionCommentary[301] = ""
+
+LT_Question[302] = ""
+LT_Answer[302] = ""
+LT_QuestionCommentary[302] = ""
+
+LT_Question[303] = ""
+LT_Answer[303] = ""
+LT_QuestionCommentary[303] = ""
+
+LT_Question[304] = ""
+LT_Answer[304] = ""
+LT_QuestionCommentary[304] = ""
+
+LT_Question[305] = ""
+LT_Answer[305] = ""
+LT_QuestionCommentary[305] = ""
+
+LT_Question[306] = ""
+LT_Answer[306] = ""
+LT_QuestionCommentary[306] = ""
+
+LT_Question[307] = ""
+LT_Answer[307] = ""
+LT_QuestionCommentary[307] = ""
+
+LT_Question[308] = ""
+LT_Answer[308] = ""
+LT_QuestionCommentary[308] = ""
+
+LT_Question[309] = ""
+LT_Answer[309] = ""
+LT_QuestionCommentary[309] = ""
+
+LT_Question[310] = ""
+LT_Answer[310] = ""
+LT_QuestionCommentary[310] = ""
+
+LT_Question[311] = ""
+LT_Answer[311] = ""
+LT_QuestionCommentary[311] = ""
+
+LT_Question[312] = ""
+LT_Answer[312] = ""
+LT_QuestionCommentary[312] = ""
+
+LT_Question[313] = ""
+LT_Answer[313] = ""
+LT_QuestionCommentary[313] = ""
+
+LT_Question[314] = ""
+LT_Answer[314] = ""
+LT_QuestionCommentary[314] = ""
+
+LT_Question[315] = ""
+LT_Answer[315] = ""
+LT_QuestionCommentary[315] = ""
+
+LT_Question[316] = ""
+LT_Answer[316] = ""
+LT_QuestionCommentary[316] = ""
+
+LT_Question[317] = ""
+LT_Answer[317] = ""
+LT_QuestionCommentary[317] = ""
+
+LT_Question[318] = ""
+LT_Answer[318] = ""
+LT_QuestionCommentary[318] = ""
+
+LT_Question[319] = ""
+LT_Answer[319] = ""
+LT_QuestionCommentary[319] = ""
+
+LT_Question[320] = ""
+LT_Answer[320] = ""
+LT_QuestionCommentary[320] = ""
+
+LT_Question[321] = ""
+LT_Answer[321] = ""
+LT_QuestionCommentary[321] = ""
+
+LT_Question[322] = ""
+LT_Answer[322] = ""
+LT_QuestionCommentary[322] = ""
+
+LT_Question[323] = ""
+LT_Answer[323] = ""
+LT_QuestionCommentary[323] = ""
+
+LT_Question[324] = ""
+LT_Answer[324] = ""
+LT_QuestionCommentary[324] = ""
+
+LT_Question[325] = ""
+LT_Answer[325] = ""
+LT_QuestionCommentary[325] = ""
+
+LT_Question[326] = ""
+LT_Answer[326] = ""
+LT_QuestionCommentary[326] = ""
+
+LT_Question[327] = ""
+LT_Answer[327] = ""
+LT_QuestionCommentary[327] = ""
+
+LT_Question[328] = ""
+LT_Answer[328] = ""
+LT_QuestionCommentary[328] = ""
+
+LT_Question[329] = ""
+LT_Answer[329] = ""
+LT_QuestionCommentary[329] = ""
+
+LT_Question[330] = ""
+LT_Answer[330] = ""
+LT_QuestionCommentary[330] = ""
+
+LT_Question[331] = ""
+LT_Answer[331] = ""
+LT_QuestionCommentary[331] = ""
+
+LT_Question[332] = ""
+LT_Answer[332] = ""
+LT_QuestionCommentary[332] = ""
+
+LT_Question[333] = ""
+LT_Answer[333] = ""
+LT_QuestionCommentary[333] = ""
+
+LT_Question[334] = ""
+LT_Answer[334] = ""
+LT_QuestionCommentary[334] = ""
+
+LT_Question[335] = ""
+LT_Answer[335] = ""
+LT_QuestionCommentary[335] = ""
+
+LT_Question[336] = ""
+LT_Answer[336] = ""
+LT_QuestionCommentary[336] = ""
+
+LT_Question[337] = ""
+LT_Answer[337] = ""
+LT_QuestionCommentary[337] = ""
+
+LT_Question[338] = ""
+LT_Answer[338] = ""
+LT_QuestionCommentary[338] = ""
+
+LT_Question[339] = ""
+LT_Answer[339] = ""
+LT_QuestionCommentary[339] = ""
+
+LT_Question[340] = ""
+LT_Answer[340] = ""
+LT_QuestionCommentary[340] = ""
+
+LT_Question[341] = ""
+LT_Answer[341] = ""
+LT_QuestionCommentary[341] = ""
+
+LT_Question[342] = ""
+LT_Answer[342] = ""
+LT_QuestionCommentary[342] = ""
+
+LT_Question[343] = ""
+LT_Answer[343] = ""
+LT_QuestionCommentary[343] = ""
+
+LT_Question[344] = ""
+LT_Answer[344] = ""
+LT_QuestionCommentary[344] = ""
+
+LT_Question[345] = ""
+LT_Answer[345] = ""
+LT_QuestionCommentary[345] = ""
+
+LT_Question[346] = ""
+LT_Answer[346] = ""
+LT_QuestionCommentary[346] = ""
+
+LT_Question[347] = ""
+LT_Answer[347] = ""
+LT_QuestionCommentary[347] = ""
+
+LT_Question[348] = ""
+LT_Answer[348] = ""
+LT_QuestionCommentary[348] = ""
+
+LT_Question[349] = ""
+LT_Answer[349] = ""
+LT_QuestionCommentary[349] = ""
+
+LT_Question[350] = ""
+LT_Answer[350] = ""
+LT_QuestionCommentary[350] = ""
+
+LT_Question[351] = ""
+LT_Answer[351] = ""
+LT_QuestionCommentary[351] = ""
+
+LT_Question[352] = ""
+LT_Answer[352] = ""
+LT_QuestionCommentary[352] = ""
+
+LT_Question[353] = ""
+LT_Answer[353] = ""
+LT_QuestionCommentary[353] = ""
+
+LT_Question[354] = ""
+LT_Answer[354] = ""
+LT_QuestionCommentary[354] = ""
+
+LT_Question[355] = ""
+LT_Answer[355] = ""
+LT_QuestionCommentary[355] = ""
+
+LT_Question[356] = ""
+LT_Answer[356] = ""
+LT_QuestionCommentary[356] = ""
+
+LT_Question[357] = ""
+LT_Answer[357] = ""
+LT_QuestionCommentary[357] = ""
+
+LT_Question[358] = ""
+LT_Answer[358] = ""
+LT_QuestionCommentary[358] = ""
+
+LT_Question[359] = ""
+LT_Answer[359] = ""
+LT_QuestionCommentary[359] = ""
+
+LT_Question[360] = ""
+LT_Answer[360] = ""
+LT_QuestionCommentary[360] = ""
+
+LT_Question[361] = ""
+LT_Answer[361] = ""
+LT_QuestionCommentary[361] = ""
+
+LT_Question[362] = ""
+LT_Answer[362] = ""
+LT_QuestionCommentary[362] = ""
+
+LT_Question[363] = ""
+LT_Answer[363] = ""
+LT_QuestionCommentary[363] = ""
+
+LT_Question[364] = ""
+LT_Answer[364] = ""
+LT_QuestionCommentary[364] = ""
+
+LT_Question[365] = ""
+LT_Answer[365] = ""
+LT_QuestionCommentary[365] = ""
+
+LT_Question[366] = ""
+LT_Answer[366] = ""
+LT_QuestionCommentary[366] = ""
+
+LT_Question[367] = ""
+LT_Answer[367] = ""
+LT_QuestionCommentary[367] = ""
+
+LT_Question[368] = ""
+LT_Answer[368] = ""
+LT_QuestionCommentary[368] = ""
+
+LT_Question[369] = ""
+LT_Answer[369] = ""
+LT_QuestionCommentary[369] = ""
+
+LT_Question[370] = ""
+LT_Answer[370] = ""
+LT_QuestionCommentary[370] = ""
+
+LT_Question[371] = ""
+LT_Answer[371] = ""
+LT_QuestionCommentary[371] = ""
+
+LT_Question[372] = ""
+LT_Answer[372] = ""
+LT_QuestionCommentary[372] = ""
+
+LT_Question[373] = ""
+LT_Answer[373] = ""
+LT_QuestionCommentary[373] = ""
+
+LT_Question[374] = ""
+LT_Answer[374] = ""
+LT_QuestionCommentary[374] = ""
+
+LT_Question[375] = ""
+LT_Answer[375] = ""
+LT_QuestionCommentary[375] = ""
+
+LT_Question[376] = ""
+LT_Answer[376] = ""
+LT_QuestionCommentary[376] = ""
+
+LT_Question[377] = ""
+LT_Answer[377] = ""
+LT_QuestionCommentary[377] = ""
+
+LT_Question[378] = ""
+LT_Answer[378] = ""
+LT_QuestionCommentary[378] = ""
+
+LT_Question[379] = ""
+LT_Answer[379] = ""
+LT_QuestionCommentary[379] = ""
+
+LT_Question[380] = ""
+LT_Answer[380] = ""
+LT_QuestionCommentary[380] = ""
+
+LT_Question[381] = ""
+LT_Answer[381] = ""
+LT_QuestionCommentary[381] = ""
+
+LT_Question[382] = ""
+LT_Answer[382] = ""
+LT_QuestionCommentary[382] = ""
+
+LT_Question[383] = ""
+LT_Answer[383] = ""
+LT_QuestionCommentary[383] = ""
+
+LT_Question[384] = ""
+LT_Answer[384] = ""
+LT_QuestionCommentary[384] = ""
+
+LT_Question[385] = ""
+LT_Answer[385] = ""
+LT_QuestionCommentary[385] = ""
+
+LT_Question[386] = ""
+LT_Answer[386] = ""
+LT_QuestionCommentary[386] = ""
+
+LT_Question[387] = ""
+LT_Answer[387] = ""
+LT_QuestionCommentary[387] = ""
+
+LT_Question[388] = ""
+LT_Answer[388] = ""
+LT_QuestionCommentary[388] = ""
+
+LT_Question[389] = ""
+LT_Answer[389] = ""
+LT_QuestionCommentary[389] = ""
+
+LT_Question[390] = ""
+LT_Answer[390] = ""
+LT_QuestionCommentary[390] = ""
+
+LT_Question[391] = ""
+LT_Answer[391] = ""
+LT_QuestionCommentary[391] = ""
+
+LT_Question[392] = ""
+LT_Answer[392] = ""
+LT_QuestionCommentary[392] = ""
+
+LT_Question[393] = ""
+LT_Answer[393] = ""
+LT_QuestionCommentary[393] = ""
+
+LT_Question[394] = ""
+LT_Answer[394] = ""
+LT_QuestionCommentary[394] = ""
+
+LT_Question[395] = ""
+LT_Answer[395] = ""
+LT_QuestionCommentary[395] = ""
+
+LT_Question[396] = ""
+LT_Answer[396] = ""
+LT_QuestionCommentary[396] = ""
+
+LT_Question[397] = ""
+LT_Answer[397] = ""
+LT_QuestionCommentary[397] = ""
+
+LT_Question[398] = ""
+LT_Answer[398] = ""
+LT_QuestionCommentary[398] = ""
+
+LT_Question[399] = ""
+LT_Answer[399] = ""
+LT_QuestionCommentary[399] = ""
+
---
+++ Resources/test_questions.lua
@@ -0,0 +1,45 @@
+--[[
+questions.lua
+Question and Answer data for LOTRivia by Carentil of Windfola
+If you use this content please give credit for where you've taken it from.
+]]--
+
+-- Shove these into global space so the plugin can see them too.
+--
+
+_G.LT_Question = {}
+_G.LT_Answer = {}
+_G.LT_QuestionCommentary = {}
+
+LT_Question[1] = "Who is Batman's sidekick?"
+LT_Answer[1] = "Robin"
+LT_QuestionCommentary[1] = ""
+
+LT_Question[2] = "What is the largest planet in the solar system?"
+LT_Answer[2] = "Jupiter"
+LT_QuestionCommentary[2] = ""
+
+LT_Question[3] = "Which planet is closest to the Sun?"
+LT_Answer[3] = "Mercury"
+LT_QuestionCommentary[3] = ""
+
+LT_Question[4] = "What is the capital of the state of California?"
+LT_Answer[4] = "Sacramento"
+LT_QuestionCommentary[4] = ""
+
+LT_Question[5] = "If two's company, what's three?"
+LT_Answer[5] = "A crowd."
+LT_QuestionCommentary[5] = ""
+
+LT_Question[6] = "What is the second planet from the sun?"
+LT_Answer[6] = "Venus"
+LT_QuestionCommentary[6] = ""
+
+LT_Question[7] = "If I have five apples and I give you two, how many apples do I have left?"
+LT_Answer[7] = "Three"
+LT_QuestionCommentary[7] = ""
+
+LT_Question[8] = "What was the Lion King's name?"
+LT_Answer[8] = "Simba"
+LT_QuestionCommentary[8] = ""
+
